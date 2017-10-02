4ce39d9d365e754b1ecfd9c774cddc27272d5c61
Leif Denby <leif@denby.eu>
2017-10-02 13:18:33 +0100

    Add talks and posters

---
+++ docs/talks_and_posters.md
@@ -0,0 +1,13 @@
+
+# Talks and posters
+
+Below are PDFs for talks and poster which have been given during GENESIS:
+
+- [2017 Delft](files/posters/2017_Delft.pdf)
+- [2017-02-22 MOAP Exeter](files/posters/2017-02-22_MOAP_Exeter.pdf)
+- [2017-05-08 Convection Seminar  Basic convective element](files/presentations/2017-05-08_Convection_Seminar__Basic_convective_element.pdf)
+- [2017-05-11 Chris Bretherton visit Leeds](files/presentations/2017-05-11_Chris_Bretherton_visit_Leeds.pdf)
+- [2017-05-02 GENESIS update](files/presentations/2017-05-02 GENESIS update.pdf)
+- [2017-08-06 UK Fluids Conference Leeds](files/presentations/2017-08-06_UK_Fluids_Conference_Leeds.pdf)
+
+Please in get in touch on <l.c.denby@leeds.ac.uk>
---
+++ update-talks-and-posters.py
@@ -0,0 +1,57 @@
+import glob
+import os
+import shutil
+
+BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+DEST_BASE_DIR = os.path.join("docs", "files")
+BASE_TEMPLATE = """
+# Talks and posters
+
+Below are PDFs for talks and posters which have been given during GENESIS:
+
+{links_rendered}
+
+Please in get in touch on <l.c.denby@leeds.ac.uk>
+"""
+
+
+def copy_files(desc, dest_dir, source_glob):
+    files = []
+
+    print "Copying files from {}...".format(desc)
+
+    START_DIR = os.path.curdir
+
+    if not os.path.exists(os.path.join(DEST_BASE_DIR, dest_dir)):
+        os.makedirs(os.path.join(DEST_BASE_DIR, dest_dir))
+
+    source_dir = os.path.join(BASE_DIR, source_glob)
+
+    dirs = glob.glob(os.path.join(source_dir, "*.pdf"))
+    for d in dirs:
+        name = d.split('/')[-3]
+        fn_new = os.path.join(DEST_BASE_DIR, dest_dir, "{}.pdf".format(name))
+        print "\t{} \n\t -> {}".format(os.path.relpath(d), fn_new)
+        shutil.copy(d, fn_new)
+        files.append((name.replace("_", " "), fn_new.replace("docs/", "")))
+
+    os.chdir(START_DIR)
+
+    return files
+
+def build_markdown(files):
+    LINK_TEMPLATE = "- [{name}]({url})"
+    links_rendered = "\n".join([LINK_TEMPLATE.format(name=name, url=url) for (name, url) in files])
+
+    return BASE_TEMPLATE.format(links_rendered=links_rendered)
+
+if __name__ == "__main__":
+    files = []
+    files += copy_files("posters at conferences", 'posters/', "conferences/*/poster")
+    files += copy_files("posters for other trips", 'posters/', "trips/*/poster")
+    files += copy_files("presentations at meetings", 'presentations/', "meetings/*/presentation")
+    files += copy_files("presentations at conferences", 'presentations/', "conferences/*/presentation")
+
+    print("Rendering template")
+    with open(os.path.join("docs", "talks_and_posters.md"), "w") as fh:
+        fh.write(build_markdown(files))
